27d3b6b3a59eafc49050fc1469769b762a1d9f96
caseymajewski <147757352+caseymajewski@users.noreply.github.com>
2023-12-05 15:50:40 -0500

    updating tkinter code to a class and __init__ method

--- tkinter_code.py
+++ tkinter_code.py
@@ -1,15 +1,33 @@
 from tkinter import *
+from tkinter import ttk
 
-# before we do anything else we have to create the window that pops up
+class Tkinter():
 
-# root widget set equal to tk()
-root = Tk()
+    def __init__(self, screenname, basename = classname, usetk, sync, use):
 
-# creating label widget
-myLabel = Label(root, text = "Welcome to Terrapin Water Tracker")
+        self.screenname = None
+        self.basename = None
+        self.classname = 'Tk'
+        self.usetk = True
+        self.sync = False
+        self.use = None
+    
+    def widget(self):
 
-# puts label on the screen
-myLabel.pack 
+        # before we do anything else we have to create the window that pops up
 
-# our root widget is included in the main loop of the program
-root.mainloop()
+        # root widget set equal to tk()
+        root = Tk()
+
+        # creating label widget
+        myLabel = Label(root, text = "Welcome to Terrapin Water Tracker")
+
+        # puts label on the screen
+        myLabel.pack 
+
+        # our root widget is included in the main loop of the program
+        root.mainloop()
+
+
+
+
