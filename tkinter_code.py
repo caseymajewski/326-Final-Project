8e368aa19ae7ae22165291d5bdf85a053bb62cdf
caseymajewski <147757352+caseymajewski@users.noreply.github.com>
2023-12-07 16:12:06 -0500

    update advice from ta's

--- tkinter_code.py
+++ tkinter_code.py
@@ -1,5 +1,14 @@
 from tkinter import *
 
+# multiple screens: multiple classes and each class is a "page" (frame) and then have a button that flips from one page to another, but also have a main class for the main part of it. 
+# when you put in a button you add in a commmand and a controller and then the command says if you click it it'll show frame and the parameters are classes that represent diff pages. Instead of a button,
+# you would have
+# have a counter and a sum and every time the sum equals a certain amount it goes to a new page, counter adds however much water the user drank. 
+# coding syntax: import tkinter as tk
+
+# inside of the specific page you are making, make the main class the home page, inside init function of each class (frame/page), use this as it defines what is happening on this page
+# need a function that changes between each frame on the main page, the main page is the one you are going to be going back and forth from
+Tkinter.Button(self, text = "cups of water", command = lambda: controller.show_frame(Tkinter))
 class Tkinter():
 
     def __init__(self, screenname, basename, classname, usetk, sync, use):
