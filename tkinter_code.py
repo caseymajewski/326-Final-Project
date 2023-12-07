b39ff98a4607a7eb16dbe888c229bc6f80b8272b
caseymajewski <147757352+caseymajewski@users.noreply.github.com>
2023-12-07 16:42:59 -0500

    update advice

--- tkinter_code.py
+++ tkinter_code.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 
 # multiple screens: multiple classes and each class is a "page" (frame) and then have a button that flips from one page to another, but also have a main class for the main part of it. 
 # when you put in a button you add in a commmand and a controller and then the command says if you click it it'll show frame and the parameters are classes that represent diff pages. Instead of a button,
@@ -8,8 +9,39 @@ from tkinter import *
 
 # inside of the specific page you are making, make the main class the home page, inside init function of each class (frame/page), use this as it defines what is happening on this page
 # need a function that changes between each frame on the main page, the main page is the one you are going to be going back and forth from
-Tkinter.Button(self, text = "cups of water", command = lambda: controller.show_frame(Tkinter))
-class Tkinter():
+
+# Tkinter.Button(self, text = "cups of water", command = lambda: controller.show_frame(Tkinter))
+
+#syntax for switching frames
+
+# this class basically inherits from the tkinter module as a subclass of the tkinter, which is why there are parameters in the class
+class MainPage(tk.Tk):
+
+    #Thompson helped us with this plzzzz
+    def __init__(self, *args, **kwargs): #asteriks are shortcuts, normally would pass in variables, instead of limiting to just x,y you can do asterik args which takes in a sample argument. Think of SQL, * allow you to put an unlimited amount of variables in there. Ifyou were to do the sum of *args and pass in 3 numbers it will add 3 numbers together. Keywords of args and kwargs mean: --------.
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(self)
+
+        self.frames = {}
+
+        container.grid_rowconfigure or .grid_columnconfigure or .grid_packconfigure #backbone of page switching function 
+
+        for i in (frame1, frame2, frame3): #change framex to whatever class we created for each frame
+            frame = i(container, self)
+            self.frames[i] = frame
+            frame.grid(row = 1, column = 1) # change the grid numbers
+
+    def show_frame(self, x):
+        frame = self.frames[x]
+        # next line depends on how we are going about it
+
+        frame.#something = self.frames
+
+
+
+'''class Tkinter():
 
     def __init__(self, screenname, basename, classname, usetk, sync, use):
 
@@ -35,5 +67,5 @@ class Tkinter():
 
         # our root widget is included in the main loop of the program
         root.mainloop()
-
+'''
 
