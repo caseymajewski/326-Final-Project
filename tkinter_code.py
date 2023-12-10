dc2636a9ae4333fa81eb5c7133c6408a65d69096
caseymajewski <147757352+caseymajewski@users.noreply.github.com>
2023-12-09 23:32:59 -0500

    Update tkinter_code.py

--- tkinter_code.py
+++ tkinter_code.py
@@ -29,7 +29,7 @@ class MainPage(tk.Tk):
 
         #container.grid_rowconfigure or .grid_columnconfigure or .grid_packconfigure #backbone of page switching function 
 
-        for f in (Frame1): #frame3): #change framex to whatever class we created for each frame
+        for f in (Frame1, Frame2): #frame3): #change framex to whatever class we created for each frame
             frame = f(container, self)
             self.frames[f] = frame
             frame.grid(row = 1, column = 1, sticky = "nsew") # sticky determines where to position the widget in its cell, and the string contains n = north, s = south, e = east, w = west
@@ -42,15 +42,103 @@ class MainPage(tk.Tk):
         # next line depends on how we are going about it
 
 class Frame1(tk.Frame):
+    ''' Homepage'''
 
     def __init__(self, parent, controller):
 
         tk.Frame.__init__(self, parent)
+
         self.controller = controller
 
+        self.load_images()
+
+        # is there a way for us to use inheritance here as well? the only thing that changes between classes is the button takes you to the next frame in the sequence
         button = tk.Button(self, command = lambda: controller.show_frame(Frame2))
+
         button.grid(row = 1, column = 0, pady = 10, padx = 10)
 
+    def load_image(self):
+
+        image_path = "C:/Users/casey/OneDrive/Documents/GitHub/326-Final-Project/MainPage().png"
+
+        self.image = PhotoImage(file = image_path)
+
+
+class Frame2(Frame1):
+    '''Info Page (Enter your information below). '''
+    # we need to add more for this one than others because it takes user input so likely needs more than just a button
+
+    def __init__(self, parent, controller):
+
+        super().__init__(parent, controller)
+
+        image_path_2 = "C:/Users/casey/OneDrive/Documents/GitHub/326-Final-Project/Info.png"
+        self.load_image(image_path_2)
+
+        button2 = tk.Button(self, command = lambda: controller.show_frame(Frame3))
+        button2.grid(row = 1, column = 0, pady = 10, padx = 10)
+
+class Frame3(Frame1):
+    '''Log Water Screen (takes you to 10%)'''
+
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+        image_path_3 = ""
+        self.load_image(image_path_3)
+
+        button3 = tk.Button(self, command = lambda: controller.show_frame(Frame4))
+        button3.grid(row = 1, column = 0, pady = 10, padx = 10)
+
+class Frame4(Frame1):
+    '''10% there'''
+
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+        image_path_4 = ""
+        self.load_image(image_path_4)
+
+        button4 = tk.Button(self, command = lambda: controller.show_frame(Frame5))
+        button4.grid(row = 1, column = 0, pady = 10, padx = 10)
+
+class Frame5(Frame1):
+    '''20% there'''
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+        image_path_5 = ""
+        self.load_image(image_path_5)
+
+        button5 = tk.Button(self, command = lambda: controller.show_frame(Frame6))
+        button5.grid(row = 1, column = 0, pady = 10, padx = 10)
+
+class Frame6(Frame1):
+    '''30% there'''
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+        image_path_6 = ""
+        self.load_image(image_path_6)
+
+        button6 = tk.Button(self, command = lambda: controller.show_frame(Frame7))
+        button6.grid(row = 1, column = 0, pady = 10, padx = 10)
+
+class Frame7(Frame1):
+    '''40% there'''
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+        image_path_7 = ""
+        self.load_image(image_path_7)
+
+        button7 = tk.Button(self, command = lambda: controller.show_frame(Frame8))
+        button7.grid(row = 1, column = 0, pady = 10, padx = 10)
+
+class Frame8(Frame1):
+    '''50% there'''
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
 
 if __name__ == "__main__":
 
