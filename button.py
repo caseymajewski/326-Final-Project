ae3706c03763ea99b5cfcf6763fea1e337af1ec4
caseymajewski <147757352+caseymajewski@users.noreply.github.com>
2023-12-14 15:53:59 -0500

    update button.py

--- button.py
+++ button.py
@@ -2,36 +2,6 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
-class Button():
-    def __init__(self):
-        root = tk.Tk()
-
-        root.geometry('1920x1080')
-        frame = Frame(root)
-        frame.pack()
-        frame.place(anchor = 'center', relx= 0.5, rely = 0.5)
-
-        img = ImageTk.PhotoImage(Image.open("20%.png"))
-
-        label = Label(frame, image = img)
-        label.pack()
-
-        I_Drank_Water_btn = tk.Button(root, text = "I Drank Water")
-        I_Drank_Water_btn.place(relx = 0.75, rely = 0.5, anchor = 'center')
-        Exit = tk.Button(root, text = "Exit", command = root.destroy)
-        Exit.pack()
-
-        root.mainloop()
-
-if __name__ == "__main__":
-    b = Button()
-
-'''
-
-from tkinter import *
-import tkinter as tk
-from PIL import ImageTk, Image
-
 class ImageButtonApp:
     def __init__(self, image_path):
         root = tk.Tk()
@@ -53,3 +23,37 @@ class ImageButtonApp:
         Exit.place(relx=0.75, rely=0.6, anchor='center')
 
         root.mainloop()
+
+if __name__ == "__main__":
+
+    image_path = ('10%.png')'''
+
+from tkinter import *
+import tkinter as tk
+from PIL import ImageTk, Image
+
+class ImageButtonApp:
+    def __init__(self, image_path):
+        root = tk.Tk()
+
+        root.geometry('1920x1080')
+        frame = Frame(root)
+        frame.pack()
+        frame.place(anchor='center', relx=0.5, rely=0.5)
+
+        img = ImageTk.PhotoImage(Image.open(image_path))
+
+        label = Label(frame, image=img)
+        label.pack()
+
+        I_Drank_Water_btn = tk.Button(frame, text="I Drank Water")
+        I_Drank_Water_btn.place(relx=0.90, rely=0.5, anchor='center')
+
+        Exit = tk.Button(frame, text="Exit", command=root.destroy)
+        Exit.place(relx=0.90, rely=0.6, anchor='center')
+
+        root.mainloop()
+
+if __name__ == "__main__":
+    image_path = '10%.png'  # Replace with the correct image file path
+    app = ImageButtonApp(image_path)
