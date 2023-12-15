2f6e897f0360a5b62890738404911a8b31a5a6c1
stepht0120 <ston1@umd.edu>
2023-12-14 20:01:43 -0500

    Update pytest.py
    more edits on tests

--- pytest.py
+++ pytest.py
@@ -20,10 +20,10 @@ def test_terrarium():
 
     bmr = water_calc.calc_BMR.bmr
     assert water_calc.calc_BMR(bmr, int)
-    assert water_calc.calc_BMR(sex = "m", weight = 180, height = 70, age = 35) == 1838.4
-    assert water_calc.calc_BMR(sex = "f", weight = 130, height = 64, age = 25) == 1403.8
-    assert water_calc.calc_BMR()
-    assert water_calc.calc_BMR()
+    assert water_calc.calc_BMR(sex = "m", weight = 180, height = 70, age = 35) == 1838.4, "Test failed: Formula should equal 1838.4 BMR"
+    assert water_calc.calc_BMR(sex = "f", weight = 130, height = 64, age = 25) == 1403.8, "Test failed: Formula should equal 1404.8 BMR"
+    assert water_calc.calc_BMR(sex = "m", weight = 150, height = 65, age = 25) == 1656, "Test failed: Formula should equal 1656 BMR"
+    assert water_calc.calc_BMR(sex = "f", weight = 140, height = 69, age = 40) == 1400.3, "Test failed: Formula should equal 1400.3 BMR"
 
     adjust = water_calc.adjust_for_activity_level.TDEE
     assert water_calc.adjust_for_activity_level(adjust, int)
@@ -32,6 +32,8 @@ def test_terrarium():
     assert water_calc.adjust_for_activity_level(bmr = 1403.8, activity_multipliers = 3) == 2175.89
     assert water_calc.adjust_for_activity_level(activity_multipliers = 4)
     assert water_calc.adjust_for_activity_level(activity_multipliers = 5)
+
+
 
     water_tracker = tm.WaterTracker
     percentage = water_tracker.check_water_intake(percentage, float) and percentage > 0
