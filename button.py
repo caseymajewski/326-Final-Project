ae8a0af3e8190e02a4299da16f4c0219141f4c72
dhabash <123644891+dhabash@users.noreply.github.com>
2023-12-15 00:04:21 -0500

    delete exit button

--- button.py
+++ button.py
@@ -12,9 +12,6 @@ class ImageButtonApp(tk.Frame):
         self.drink_water_button.pack()
         self.drink_water_button.place(relheight=.12, relwidth=0.2, relx=0.90, rely=0.5, anchor='center')
 
-        self.exit_button = tk.Button(self, text="Exit", fg='#FFCCCB', bg='#CB3737', font=("Times New Roman Bold", 10), command=self.quit)
-        self.exit_button.place(relx=0.90, rely=0.6, anchor='center')
-
     def next_image(self):
         self.image_index = (self.image_index + 1) % len(self.image_paths)
         self.img = ImageTk.PhotoImage(Image.open(self.image_paths[self.image_index]))
