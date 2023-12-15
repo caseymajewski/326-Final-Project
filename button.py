1779114b748b062897d2dfbb3712092622d98b05
dhabash <123644891+dhabash@users.noreply.github.com>
2023-12-15 00:02:22 -0500

    Update button.py

--- button.py
+++ button.py
@@ -5,7 +5,6 @@ from PIL import ImageTk, Image
 class ImageButtonApp(tk.Frame):
     def __init__(self, parent, image_paths, drink_water_command, **kwargs):
         super().__init__(parent, **kwargs)
-        self.parent = parent
         self.image_paths = image_paths
         self.image_index = 0
 
@@ -13,7 +12,7 @@ class ImageButtonApp(tk.Frame):
         self.drink_water_button.pack()
         self.drink_water_button.place(relheight=.12, relwidth=0.2, relx=0.90, rely=0.5, anchor='center')
 
-        self.exit_button = tk.Button(self, text="Exit", fg='#FFCCCB', bg='#CB3737', font=("Times New Roman Bold", 10), command=self.parent.destroy)
+        self.exit_button = tk.Button(self, text="Exit", fg='#FFCCCB', bg='#CB3737', font=("Times New Roman Bold", 10), command=self.quit)
         self.exit_button.place(relx=0.90, rely=0.6, anchor='center')
 
     def next_image(self):
